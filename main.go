97fadcc8f8bd91498314954b0641e00d3335fcf1
VictoriqueMoe <loli@victorique.moe>
2024-06-16 21:37:21 +0100

    WIP

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
@@ -9,25 +10,118 @@ import (
 	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	waifuVault "github.com/waifuvault/waifuVault-go-api/pkg"
+	waifuMod "github.com/waifuvault/waifuVault-go-api/pkg/mod"
 	"image/color"
+	"net/http"
+	"os"
 )
 
 func main() {
+	api := waifuVault.NewWaifuvaltApi(http.Client{})
+
 	waifuVaultApp := app.New()
 	w := waifuVaultApp.NewWindow("WaifuVault")
 	w.Resize(fyne.NewSize(500, 100))
 	title := canvas.NewText("Open file", color.White)
 	file := binding.NewString()
-	checkButton := widget.NewButton("Choose file", func() {
+
+	checkButton := widget.NewButton("Choose or drag and drop file", func() {
 		chooseDirectory(w, file)
 	})
+
+	w.SetOnDropped(func(position fyne.Position, uris []fyne.URI) {
+		if len(uris) != 1 {
+			dialog.ShowError(errors.New("only one file is allowed"), w)
+			return
+		}
+		path := uris[0].Path()
+		file.Set(path)
+	})
+
+	resultText := binding.NewString()
+
+	copyButton := widget.NewButtonWithIcon("copy Url", theme.ContentCopyIcon(), func() {
+		if content, err := resultText.Get(); err == nil {
+			w.Clipboard().SetContent(content)
+		}
+	})
+	copyButton.Disable()
+
+	var uploadButton *widget.Button
+	uploadButton = widget.NewButton("Upload file", func() {
+		fileToUpload, _ := file.Get()
+		if fileToUpload == "" {
+			dialog.ShowError(errors.New("no file selected"), w)
+			return
+		}
+		uploadButton.Disable()
+		uploadFile(fileToUpload, w, api, func(result *waifuMod.WaifuResponse[string], err error) {
+			if err != nil {
+				uploadButton.Enable()
+				dialog.ShowError(err, w)
+				return
+			}
+
+			/*url := result.URL
+			token := result.Token*/
+
+			copyButton.Enable()
+			uploadButton.Enable()
+		})
+	})
+	uploadButton.Disable()
+
+	file.AddListener(binding.NewDataListener(func() {
+		str, _ := file.Get()
+		if str != "" {
+			uploadButton.Enable()
+		}
+	}))
+
+	selectedFileLabel := widget.NewLabelWithData(file)
 	windowContent := container.NewVBox(
 		createCenteredComponents(title),
 		container.NewVBox(checkButton),
+		createCenteredComponents(selectedFileLabel),
+		container.NewVBox(uploadButton),
 	)
-	w.SetContent(windowContent)
+
+	txtWid := widget.NewLabelWithData(resultText)
+
+	bottomBox := container.NewVBox(
+		layout.NewSpacer(),
+		copyButton,
+	)
+
+	content := container.NewBorder(windowContent, bottomBox, nil, nil, txtWid)
+
+	w.SetContent(content)
 	w.ShowAndRun()
+}
+
+func uploadFile(
+	path string,
+	w fyne.Window,
+	api waifuMod.Waifuvalt,
+	callBack func(*waifuMod.WaifuResponse[string], error),
+) {
+
+	fileStruc, err := os.Open(path)
+	if err != nil {
+		callBack(nil, err)
+		return
+	}
+	resp, err := api.UploadFile(context.TODO(), waifuMod.WaifuvaultPutOpts{
+		File: fileStruc,
+	})
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+	callBack(resp, nil)
 }
 
 func chooseDirectory(w fyne.Window, h binding.String) {
@@ -49,9 +143,7 @@ func chooseDirectory(w fyne.Window, h binding.String) {
 			return
 		}
 
-		fmt.Println(file.URI())
 		saveDir = file.URI().Path()
-
 		err = h.Set(saveDir)
 		if err != nil {
 			dialog.ShowError(err, w)
