3a0f7192f3d48a0a50fdc278d020f1a1c8e6ddd8
VictoriqueMoe <loli@victorique.moe>
2024-06-19 20:18:08 +0100

    refactor

--- main.go
+++ main.go
@@ -25,28 +25,40 @@ func main() {
 	waifuVaultApp := app.New()
 	w := waifuVaultApp.NewWindow("WaifuVault")
 	w.Resize(fyne.NewSize(500, 100))
+
 	title := canvas.NewText("Open file", color.White)
 	uploadingText := canvas.NewText("Uploading...", color.White)
 	uploadCompleteText := canvas.NewText("Upload successful", color.White)
+
 	tokenLbBind := binding.NewString()
 	urlLbBind := binding.NewString()
 	file := binding.NewString()
-	tokenLb := widget.NewLabelWithData(tokenLbBind)
-	urlLb := widget.NewLabelWithData(urlLbBind)
 
 	uploadTextContainer := createCenteredComponents(uploadingText)
 	uploadCompleteTextContainer := createCenteredComponents(uploadCompleteText)
-	urlContainer := createCenteredComponents(urlLb)
-	tokenContainer := createCenteredComponents(tokenLb)
 
-	urlContainer.Hide()
-	tokenContainer.Hide()
-	uploadTextContainer.Hide()
-	uploadCompleteTextContainer.Hide()
+	hideContainers(uploadTextContainer, uploadCompleteTextContainer)
 
-	checkButton := widget.NewButton("Choose or drag and drop file", func() {
-		chooseDirectory(w, file)
-	})
+	checkButton := createCheckButton(w, file)
+	uploadButton := createUploadButton(w, file, api, uploadTextContainer, uploadCompleteTextContainer, urlLbBind, tokenLbBind)
+	copyUrlButton, copyTokenButton := createCopyButtons(w, urlLbBind, tokenLbBind)
+
+	selectedFileLabel := widget.NewLabelWithData(file)
+	windowContent := container.NewVBox(
+		createCenteredComponents(title),
+		container.NewVBox(checkButton),
+		createCenteredComponents(selectedFileLabel),
+		container.NewVBox(uploadButton),
+		uploadTextContainer,
+		uploadCompleteTextContainer,
+	)
+
+	bottomBox := container.NewHBox(
+		layout.NewSpacer(),
+		copyUrlButton,
+		copyTokenButton,
+		layout.NewSpacer(),
+	)
 
 	w.SetOnDropped(func(position fyne.Position, uris []fyne.URI) {
 		if len(uris) != 1 {
@@ -57,21 +69,25 @@ func main() {
 		file.Set(path)
 	})
 
-	copyUrlButton := widget.NewButtonWithIcon("copy Url", theme.ContentCopyIcon(), func() {
-		if content, err := urlLbBind.Get(); err == nil {
-			w.Clipboard().SetContent(content)
-		}
+	content := container.NewBorder(windowContent, bottomBox, nil, nil)
+
+	w.SetContent(content)
+	w.ShowAndRun()
+}
+
+func hideContainers(containers ...fyne.CanvasObject) {
+	for _, c := range containers {
+		c.Hide()
+	}
+}
+
+func createCheckButton(w fyne.Window, file binding.String) *widget.Button {
+	return widget.NewButton("Choose or drag and drop file", func() {
+		chooseDirectory(w, file)
 	})
+}
 
-	copyTokenButton := widget.NewButtonWithIcon("copy Token", theme.ContentCopyIcon(), func() {
-		if content, err := tokenLbBind.Get(); err == nil {
-			w.Clipboard().SetContent(content)
-		}
-	})
-
-	copyUrlButton.Disable()
-	copyTokenButton.Disable()
-
+func createUploadButton(w fyne.Window, file binding.String, api waifuMod.Waifuvalt, uploadTextContainer, uploadCompleteTextContainer *fyne.Container, urlLbBind, tokenLbBind binding.String) *widget.Button {
 	var uploadButton *widget.Button
 	uploadButton = widget.NewButton("Upload file", func() {
 		fileToUpload, _ := file.Get()
@@ -83,21 +99,14 @@ func main() {
 		uploadButton.Disable()
 		uploadFile(fileToUpload, w, api, func(result *waifuMod.WaifuResponse[string], err error) {
 			if err != nil {
-				uploadButton.Enable()
 				dialog.ShowError(err, w)
-				uploadTextContainer.Hide()
-				uploadCompleteTextContainer.Hide()
+				resetUploadState(uploadButton, uploadTextContainer, uploadCompleteTextContainer)
 				return
 			}
 
 			urlLbBind.Set(result.URL)
 			tokenLbBind.Set(result.Token)
-
-			copyUrlButton.Enable()
-			copyTokenButton.Enable()
-			uploadButton.Enable()
-			uploadTextContainer.Hide()
-			uploadCompleteTextContainer.Show()
+			toggleUploadState(uploadButton, uploadTextContainer, uploadCompleteTextContainer)
 		})
 	})
 	uploadButton.Disable()
@@ -109,43 +118,51 @@ func main() {
 		}
 	}))
 
-	tokenLbBind.AddListener(binding.NewDataListener(func() {
-		str, _ := tokenLbBind.Get()
-		if str != "" {
-			tokenContainer.Show()
+	return uploadButton
+}
+
+func resetUploadState(uploadButton *widget.Button, uploadTextContainer, uploadCompleteTextContainer fyne.CanvasObject) {
+	uploadButton.Enable()
+	uploadTextContainer.Hide()
+	uploadCompleteTextContainer.Hide()
+}
+
+func toggleUploadState(uploadButton *widget.Button, uploadTextContainer, uploadCompleteTextContainer fyne.CanvasObject) {
+	uploadButton.Enable()
+	uploadTextContainer.Hide()
+	uploadCompleteTextContainer.Show()
+}
+
+func createCopyButtons(w fyne.Window, urlLbBind, tokenLbBind binding.String) (*widget.Button, *widget.Button) {
+	copyUrlButton := widget.NewButtonWithIcon("copy Url", theme.ContentCopyIcon(), func() {
+		if content, err := urlLbBind.Get(); err == nil {
+			w.Clipboard().SetContent(content)
 		}
-	}))
+	})
+	copyTokenButton := widget.NewButtonWithIcon("copy Token", theme.ContentCopyIcon(), func() {
+		if content, err := tokenLbBind.Get(); err == nil {
+			w.Clipboard().SetContent(content)
+		}
+	})
+
+	copyUrlButton.Disable()
+	copyTokenButton.Disable()
 
 	urlLbBind.AddListener(binding.NewDataListener(func() {
 		str, _ := urlLbBind.Get()
 		if str != "" {
-			urlContainer.Show()
+			copyUrlButton.Enable()
 		}
 	}))
 
-	selectedFileLabel := widget.NewLabelWithData(file)
-	windowContent := container.NewVBox(
-		createCenteredComponents(title),
-		container.NewVBox(checkButton),
-		createCenteredComponents(selectedFileLabel),
-		container.NewVBox(uploadButton),
-		uploadTextContainer,
-		uploadCompleteTextContainer,
-		urlContainer,
-		tokenContainer,
-	)
+	tokenLbBind.AddListener(binding.NewDataListener(func() {
+		str, _ := tokenLbBind.Get()
+		if str != "" {
+			copyTokenButton.Enable()
+		}
+	}))
 
-	bottomBox := container.NewHBox(
-		layout.NewSpacer(),
-		copyUrlButton,
-		copyTokenButton,
-		layout.NewSpacer(),
-	)
-
-	content := container.NewBorder(windowContent, bottomBox, nil, nil)
-
-	w.SetContent(content)
-	w.ShowAndRun()
+	return copyUrlButton, copyTokenButton
 }
 
 func uploadFile(
